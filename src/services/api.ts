bb26c420fcae4141bd8fa02f4c761cb83e381ddc
Weslley Fernando Teixeira Chaves <weslleyfernando71609@gmail.com>
2025-03-27 19:47:42 -0300

    feat: link do back

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "http://bet-web-nod-git-main-davicastelos-projects.vercel.app/",
   headers: {
     "Content-Type": "application/json",
   },
